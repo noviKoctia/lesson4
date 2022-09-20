b0e6e35f95d658146a5515e0ba83ee10ec7d48e9
noviKoctia <113854558+noviKoctia@users.noreply.github.com>
2022-09-20 23:19:11 +0300

    update .gitignore

--- src/test/java/org/example/exampleTest.java
+++ src/test/java/org/example/exampleTest.java
@@ -1,2 +1,11 @@
-package org.example;public class exampleTest {
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class exampleTest {
+    @Test
+    void exampleTest0() {
+        Assertions.assertTrue (2 > 1);
+    }
 }
